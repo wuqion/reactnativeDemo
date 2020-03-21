5c7cf21cb16ef54ca886b2ff970b1f1c46adfdb2
吴琼 <wuqiongaia@163.com>
2020-03-21 18:21:14 +0800

    first commit

---
+++ demo/src/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>ddd</Text>
+    </View>
+  );
+};
+
+
+export default App;
